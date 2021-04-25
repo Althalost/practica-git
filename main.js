77681ed47a7a1759884f140d068e3a6f5540da48
Pablo Lopez <pablolopez101200@gmail.com>
2021-04-25 02:40:43 -0400

    agragamos todos los archivos

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+    document.querySelector(".menu-btn").addEventListener("click", () => {
+        document.querySelector(".nav-menu").classList.toggle("show");
+    });
+    ScrollReveal().reveal('.showcase');
+    ScrollReveal().reveal('.news-cards', {delay:200});
+    ScrollReveal().reveal('.cards-banner-one', {delay:200});
+    ScrollReveal().reveal('.cards-banner-two', {delay:200});
+}
